82ef0abf296ba68ba2b16f1d7dda1b5c1a9a364b
10523771 <56820339+anjali-wq@users.noreply.github.com>
2019-11-07 21:43:40 +0530

    validation and defining

--- Python_CA1.py
+++ Python_CA1.py
@@ -32,3 +32,30 @@ class StudentRecords:
     def show_menu(self):
      
         
+        while selected_option != '1' and selected_option != '2':
+          
+            print('Press 1 to execute the add() function')
+            print('Press 2 to execute the show_all_records() function')
+           
+            selected_option = input('Enter your option > ')
+            print('You have selected: ' + selected_option)
+            if selected_option == '1':
+            
+                self.add()
+            else selected_option == '2':
+                self.show_all_records()
+         
+
+def main():
+    s = StudentRecords([
+        [7, 'A. McStudent', 'A1234567'],
+        [9, 'B. McStudent', 'B1234567'],
+        [2, 'C. McStudent', 'C1234567'],
+        [1, 'D. McStudent', 'E1234567'],
+        [3, 'E. McStudent', 'F1234567'],
+        [8, 'F. McStudent', 'G1234567'],
+        [5, 'G. McStudent', 'H1234567'],
+        [6, 'H. McStudent', 'I1234567'],
+        [99999999, 'I. McStudent', 'D1234567']
+])
+    s.show_menu()
